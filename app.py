ae60b269ddc9d2c1fd9499773f12766155b71306
ferchogx-art <ferchogx@gmail.com>
2026-09-21 12:56:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,8 @@ if prompt := st.chat_input("¿Qué rendimiento o producto deseas validar? o que 
         try:
             # Usamos el modelo ultra rápido, gratuito y 100% vigente de Groq
             completion = client.chat.completions.create(
-                model="llama-3.3-70b-versatile",
+               model="llama-3.1-8b-instant",
+
 
                 messages=[
                     {"role": "system", "content": contexto_sistema},
