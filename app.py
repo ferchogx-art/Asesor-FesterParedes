2586dd38be5da24271ba9d34b63fa2bca20e0f3b
ferchogx-art <ferchogx@gmail.com>
2026-09-21 17:35:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if not api_key:
     st.stop()
 
 client = Groq(api_key=api_key)
-MODELO_FAVORITO = "openai/gpt-oss-120b"
+MODELO_FAVORITO = "llama-3.1-8b-instant"
  # Cambiado al modelo estable oficial para evitar bloqueos
 
 # 2. Carga inteligente de fichas técnicas en formato PDF (Sueltas en GitHub)
