027c29cfe485f8039c21b26d6a341c06990d29b0
ferchogx-art <ferchogx@gmail.com>
2026-09-22 18:25:34 -0600

    Fix sorting of resultados by puntos

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def buscar_fichas(consulta, historial):
 
     if resultados:
         # Mantenemos tu ordenador original intacto
-        resultados.sort(key=lambda x: x, reverse=True)
+        resultados.sort(key=lambda x: x[0], reverse=True)
     return "".join(f"\n[Ficha: {item['origen']}]\n{item['texto']}\n" for _, item in resultados[:2])
 
 # 5. Pintar historial en pantalla
